75412ae789c542416ef4844a18654087618d5631
Matt Burke <spraints@gmail.com>
2011-11-29 11:18:27 -0500

    New rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanbfire::Application.config.secret_token = 'ccdee56518f49f39b404318cacf8db3c716ac7fd479749b4104933f877dc7f50d63d43b9cb5049b79e648ac326dc09cc055b759b98216382440b256e74d801d0'
